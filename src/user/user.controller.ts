5a69a7fa616ac0f1ea869f5d70eafe853f9466fe
ParkAsher <devasherpark@naver.com>
2024-01-06 17:07:16 +0900

    [feat] : 회원 정보 가져오기 기능 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    HttpStatus,
+    Request,
+    UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guards';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
     constructor(private readonly userService: UserService) {}
+
+    @UseGuards(JwtAuthGuard)
+    @Get('/info')
+    async getUserInfo(@Request() req) {
+        const { id, accessToken, refreshToken } = req.user;
+
+        console.log(req.user);
+
+        const user = await this.userService.getUserInfo(id);
+
+        return {
+            statusCode: HttpStatus.OK,
+            message: '회원 정보를 성공적으로 가져왔습니다.',
+            user,
+            accessToken,
+            refreshToken,
+        };
+    }
 }
