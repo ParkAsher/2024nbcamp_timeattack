425470630ad2ffd8e8b3b440c964e170291bbf60
ParkAsher <devasherpark@naver.com>
2024-01-06 12:46:18 +0900

    [feat] : 회원가입 데이터 엔티티 생성

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,4 +1,10 @@
 import {
+    IsEmail,
+    IsNotEmpty,
+    IsString,
+    IsStrongPassword,
+} from 'class-validator';
+import {
     Column,
     CreateDateColumn,
     DeleteDateColumn,
@@ -12,24 +18,37 @@ export class User {
     @PrimaryGeneratedColumn({ unsigned: true })
     id: number;
 
+    @IsNotEmpty({ message: '이메일을 입력해 주세요.' })
+    @IsEmail({}, { message: '이메일 형식에 맞지 않습니다.' })
     @Column({ unique: true })
     email: string;
 
+    @IsNotEmpty({ message: '비밀번호를 입력해 주세요.' })
+    @IsStrongPassword(
+        {},
+        {
+            message:
+                '비밀번호는 영문 알파벳 대/소문자, 숫자, 특수문자를 포함해야합니다.',
+        },
+    )
     @Column()
     password: string;
 
+    @IsNotEmpty({ message: '이름을 입력해 주세요.' })
+    @IsString({ message: '이름 형식에 맞지 않습니다.' })
     @Column()
     name: string;
 
-    @Column({ unsigned: true })
-    phone: number;
+    @IsString()
+    @Column()
+    phone: string;
 
-    @CreateDateColumn({ name: 'created_at' })
+    @CreateDateColumn()
     createdAt: Date;
 
-    @UpdateDateColumn({ name: 'updated_at' })
+    @UpdateDateColumn()
     updatedAt: Date;
 
-    @DeleteDateColumn({ name: 'deleted_at' })
+    @DeleteDateColumn()
     deletedAt: Date | null;
 }
